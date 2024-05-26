628e78a5ebfb611349e9e43a32219a8b71a8e9c7
ahmedabbasgithub <ahmedabbasschool@gmail.com>
2024-05-26 15:51:45 +0000

    Module 1: Exercise 1.1

--- main.cs
+++ main.cs
@@ -1,7 +1,27 @@
-using System;
+using static System.Console;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! Ahmed is Here!");
-  }
+class PersonalInfo
+{
+    static void Main()
+    {
+        WriteLine("Lucinda Potter");
+        WriteLine("6/24/1992");
+        WriteLine("work 000-000-0101");
+        WriteLine("cell 000-000-0189");
+        
+        WriteLine("John Smith");
+        WriteLine("8/15/1978");
+        WriteLine("work 000-111-0101");
+        WriteLine("cell 000-111-0189");
+
+        WriteLine("Chris Johnson");
+        WriteLine("12/01/1987");
+        WriteLine("work 000-222-0101");
+        WriteLine("cell 000-222-0189");
+
+        WriteLine("Rober Hall");
+        WriteLine("2/27/1949");
+        WriteLine("work 000-444-0101");
+        WriteLine("cell 000-444-0189");
+    }
 }
