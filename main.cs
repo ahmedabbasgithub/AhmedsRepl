d74e911c14eb8697b59c3aebbe2fc9a854f84364
ahmedabbasgithub <ahmedabbasschool@gmail.com>
2024-06-03 02:56:05 +0000

    Module 2: Exercise 2.2 (MoveEstimator)

--- main.cs
+++ main.cs
@@ -1,36 +1,35 @@
 using System;
 
-class InchesToCentimeterslnteractive
+class MoveEstimator
 {
-    // Named constant for the number of centimeters in an inch
-    const double CentimetersPerInch = 2.54;
-
     static void Main(string[] args)
     {
         // Execute all unit tests
-        UnitTest(1, 5);
-        UnitTest(2, 50);
-        UnitTest(3, -15);
-        UnitTest(4, 15994);
+        UnitTest(1, 10, 600, 2900.00);
+        UnitTest(2, 6.77, 15, 1245.50);
+        UnitTest(3, 56.25, 325, 9287.50);
+        UnitTest(4, 103, 3000, 21650.00);
     }
 
-    static void UnitTest(int testNumber, double inches)
+    static void UnitTest(int testNumber, double hours, double miles, double expectedOutcome)
     {
-        double actualCentimeters = inches * CentimetersPerInch;
-        string expectedCentimeters;
-
-        if (inches == 15994)
-        {
-            expectedCentimeters = "406.24.76";
-        }
-        else
-        {
-            expectedCentimeters = actualCentimeters.ToString("F1");
-        }
+        double totalFee = CalculateMovingFee(hours, miles);
 
         Console.WriteLine($"Unit Test #{testNumber}");
-        Console.WriteLine($"Required Inputs:  {inches} inches");
-        Console.WriteLine($"Expected Outcome: {expectedCentimeters} centimeters");
+        Console.WriteLine($"Required Inputs:");
+        Console.WriteLine($"{hours} hours");
+        Console.WriteLine($"{miles} miles");
+        Console.WriteLine($"Expected Outcome: ${expectedOutcome:F2}");
         Console.WriteLine();
+    }
+
+    static double CalculateMovingFee(double hours, double miles)
+    {
+        const double baseRate = 200;
+        const double hourlyRate = 150;
+        const double perMileRate = 2;
+
+        double totalFee = baseRate + (hourlyRate * hours) + (perMileRate * miles);
+        return totalFee;
     }
 }
