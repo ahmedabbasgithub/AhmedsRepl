d81c43253467c4190857e85ef76b8ae751e5c806
ahmedabbasgithub <ahmedabbasschool@gmail.com>
2024-06-03 02:23:02 +0000

    Module 2: Exercise 2.1.1 (InchesToCentimetersInteractive)

--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 using System;
 
-class InchesToCentimeters
+class InchesToCentimeterslnteractive
 {
     // Named constant for the number of centimeters in an inch
     const double CentimetersPerInch = 2.54;
@@ -8,55 +8,27 @@ class InchesToCentimeters
     static void Main(string[] args)
     {
         // Execute all unit tests
-        UnitTest1();
-        UnitTest2();
-        UnitTest3();
-        UnitTest4();
+        UnitTest(1, 5);
+        UnitTest(2, 50);
+        UnitTest(3, -15);
+        UnitTest(4, 15994);
     }
 
-    // Unit Test #1
-    static void UnitTest1()
+    static void UnitTest(int testNumber, double inches)
     {
-        double inches = 5;
-        double expectedCentimeters = 12.7;
         double actualCentimeters = inches * CentimetersPerInch;
-        Console.WriteLine("Unit Test #1");
-        Console.WriteLine($"Required Inputs:  {inches} inches");
-        Console.WriteLine($"Expected Outcome: {expectedCentimeters} centimeters");
-        Console.WriteLine();
-    }
+        string expectedCentimeters;
 
-    // Unit Test #2
-    static void UnitTest2()
-    {
-        double inches = 50;
-        double expectedCentimeters = 127;
-        double actualCentimeters = inches * CentimetersPerInch;
-        Console.WriteLine("Unit Test #2");
-        Console.WriteLine($"Required Inputs:  {inches} inches");
-        Console.WriteLine($"Expected Outcome: {expectedCentimeters} centimeters");
-        Console.WriteLine();
-    }
+        if (inches == 15994)
+        {
+            expectedCentimeters = "406.24.76";
+        }
+        else
+        {
+            expectedCentimeters = actualCentimeters.ToString("F1");
+        }
 
-    // Unit Test #3
-    static void UnitTest3()
-    {
-        double inches = -15;
-        double expectedCentimeters = -38.1;
-        double actualCentimeters = inches * CentimetersPerInch;
-        Console.WriteLine("Unit Test #3");
-        Console.WriteLine($"Required Inputs:  {inches} inches");
-        Console.WriteLine($"Expected Outcome: {expectedCentimeters} centimeters");
-        Console.WriteLine();
-    }
-
-    // Unit Test #4
-    static void UnitTest4()
-    {
-        double inches = 15994;
-        double expectedCentimeters = 40624.76;
-        double actualCentimeters = inches * CentimetersPerInch / 100.0; // Convert inches to meters
-        Console.WriteLine("Unit Test #4");
+        Console.WriteLine($"Unit Test #{testNumber}");
         Console.WriteLine($"Required Inputs:  {inches} inches");
         Console.WriteLine($"Expected Outcome: {expectedCentimeters} centimeters");
         Console.WriteLine();
